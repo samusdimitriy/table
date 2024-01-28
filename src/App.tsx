62f87a54500948764d4e4e8097368d5d964317f9
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-28 12:49:53 +0100

    Remove unused import statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AccountsTable from './components/AccountsTable';
